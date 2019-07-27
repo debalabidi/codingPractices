51e6018ad1f5bafa9f0066f5b50752628a8f4d3c
dabidi <debal.abidi@gmail.com>
2019-07-27 13:20:55 -0700

    testing2

---
+++ src/main/java/com/helloworld/Helloworld.java
@@ -0,0 +1,10 @@
+package com.helloworld;
+
+/**
+ * Created by dabidi on 7/27/19.
+ */
+public class Helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
